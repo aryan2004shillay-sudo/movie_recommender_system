fc63eb04b16c3a5373b05be299b3d5b5df284337
aryan2004shillay-sudo <aryan2004shillay@gmail.com>
2025-11-24 22:48:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import os
 import zipfile
 import pickle
+import streamlit as st
+import requests
+import streamlit.components.v1 as components
+
 
 if not os.path.exists("similarity.pkl"):
     if os.path.exists("similarity.zip"):
@@ -8,85 +12,65 @@ if not os.path.exists("similarity.pkl"):
             z.extractall(".")
     else:
         raise FileNotFoundError("similarity.zip not found.")
-        
+
 with open("similarity.pkl", "rb") as f:
     similarity = pickle.load(f)
-import streamlit as st
-import pickle
-import requests
-
-def fetch_poster(movie_id):
-     url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
-     data=requests.get(url)
-     data=data.json()
-     poster_path = data['poster_path']
-     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
-     return full_path
 
 movies = pickle.load(open("movies_list.pkl", 'rb'))
-similarity = pickle.load(open("similarity.pkl", 'rb'))
-movies_list=movies['title'].values
+movies_list = movies['title'].values
 
+# --------------------------
+# UI
+# --------------------------
 st.header("Movie Recommender System")
 
-import streamlit.components.v1 as components
+imageCarouselComponent = components.declare_component(
+    "image-carousel-component", path="frontend/public"
+)
 
-imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
-
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US"
+    data = requests.get(url).json()
+    poster_path = data['poster_path']
+    return "https://image.tmdb.org/t/p/w500/" + poster_path
 
 imageUrls = [
-    fetch_poster(1632),
-    fetch_poster(299536),
-    fetch_poster(17455),
-    fetch_poster(2830),
-    fetch_poster(429422),
-    fetch_poster(9722),
-    fetch_poster(13972),
-    fetch_poster(240),
-    fetch_poster(155),
-    fetch_poster(598),
-    fetch_poster(914),
-    fetch_poster(255709),
+    fetch_poster(1632), fetch_poster(299536), fetch_poster(17455),
+    fetch_poster(2830), fetch_poster(429422), fetch_poster(9722),
+    fetch_poster(13972), fetch_poster(240), fetch_poster(155),
+    fetch_poster(598), fetch_poster(914), fetch_poster(255709),
     fetch_poster(572154)
-   
-    ]
-
+]
 
 imageCarouselComponent(imageUrls=imageUrls, height=200)
-selectvalue=st.selectbox("Select movie from dropdown", movies_list)
+
+selectvalue = st.selectbox("Select movie from dropdown", movies_list)
 
 def recommend(movie):
-    index=movies[movies['title']==movie].index[0]
-    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
-    recommend_movie=[]
-    recommend_poster=[]
+    index = movies[movies['title'] == movie].index[0]
+    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector: vector[1])
+
+    recommend_movie = []
+    recommend_poster = []
+
     for i in distance[1:6]:
-        movies_id=movies.iloc[i[0]].id
+        movie_id = movies.iloc[i[0]].id
         recommend_movie.append(movies.iloc[i[0]].title)
-        recommend_poster.append(fetch_poster(movies_id))
+        recommend_poster.append(fetch_poster(movie_id))
+
     return recommend_movie, recommend_poster
-
-
 
 if st.button("Show Recommend"):
     movie_name, movie_poster = recommend(selectvalue)
-    col1,col2,col3,col4,col5=st.columns(5)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        st.text(movie_name[0])
-        st.image(movie_poster[0])
+        st.text(movie_name[0]); st.image(movie_poster[0])
     with col2:
-        st.text(movie_name[1])
-        st.image(movie_poster[1])
+        st.text(movie_name[1]); st.image(movie_poster[1])
     with col3:
-        st.text(movie_name[2])
-        st.image(movie_poster[2])
+        st.text(movie_name[2]); st.image(movie_poster[2])
     with col4:
-        st.text(movie_name[3])
-        st.image(movie_poster[3])
+        st.text(movie_name[3]); st.image(movie_poster[3])
     with col5:
-        st.text(movie_name[4])
-        st.image(movie_poster[4])
-
-
-
-
+        st.text(movie_name[4]); st.image(movie_poster[4])
